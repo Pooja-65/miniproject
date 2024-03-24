8f2b92c435e03b44b7d9a927f44a9111ea7c3375
Pooja-65 <poojavachar@gmail.com>
2024-03-24 12:59:10 +0530

    first commit

---
+++ hostel/admin/includes/header.php
@@ -0,0 +1,13 @@
+<div class="brand clearfix">
+<a href="#" class="logo" style="display: block; background-image: url('img/header.jpeg') ; background-size: cover; background-position: center; background-repeat: no-repeat; width:100%; height: 50px; text-align: center; line-height: 50px; color: #fff; font-size: 20px; text-decoration: none;">Hostel Management System</a>		
+<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="admin-profile.php">My Account</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
